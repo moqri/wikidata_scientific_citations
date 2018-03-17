1155ee6a292234e45f5a0cd38a98b444f5910923
moqri <moqri@ufl.edu>
2018-03-17 01:56:55 -0400

    .

---
+++ fatameh_sister_bot/drop_trailing_dot.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8  -*-
+import pywikibot
+
+site = pywikibot.Site("wikidata", "wikidata")
+repo = site.data_repository()
+
+with open('pages.txt') as f:
+    items_Q = f.read().splitlines()
+for item_Q in items_Q:
+    item = pywikibot.ItemPage(repo, item_Q)
+    item.get()
+    print item.labels['en']
+    if item.labels['en'][-1]=='.':
+        new_labels=item.labels
+        new_labels['en']=new_labels['en'][:-1]
+        item.editLabels(labels=new_labels, summary="Fixing the label imported by Fatameh.")
+
+#claim.setTarget(target)
+#item.addClaim(claim, summary=u'Adding claim')
