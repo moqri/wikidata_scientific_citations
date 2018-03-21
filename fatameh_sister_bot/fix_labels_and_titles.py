ad1eacdc56b6e27e8715d80464ee101e4d79204a
Mahdimoqri <mahdimoqri@tools-bastion-03.tools.eqiad.wmflabs>
2018-03-21 15:41:22 +0000

    .

--- fatameh_sister_bot/fix_labels_and_titles.py
+++ fatameh_sister_bot/fix_labels_and_titles.py
@@ -6,30 +6,33 @@ repo = site.data_repository()
 
 with open('pages.txt') as f:
     items_Q = f.read().splitlines()
-for item_Q in items_Q[1345:]:
-    print item_Q,
+#for item_Q in items_Q[50460:1000000]:
+for item_Q in items_Q[59230:1000000]:
+  print item_Q,
+  try:  
     item = pywikibot.ItemPage(repo, item_Q)
     item.get()
     if 'en' not in item.labels:
         continue
-    if item.labels['en'][-1]=='.' or item.labels['en'][-1]=='*': 
+    if item.labels['en'][-1]=='.' :#or item.labels['en'][-1]=='*': 
         new_labels=item.labels
 	fixed_label=item.labels['en'][:-1].strip()
-        while fixed_label[-1] =='.' or fixed_label[-1]=='*':
-		fixed_label=fixed_label[:-1]
+        #while fixed_label[-1] =='.' or fixed_label[-1]=='*':
+	#	fixed_label=fixed_label[:-1].strip()
 	new_labels['en']=fixed_label
-        item.editLabels(labels=new_labels, summary="Removing trailing dots or astrisks from the label")
+     	item.editLabels(labels=new_labels, summary="Removing the trailing dot from the label")
     
     title_claim=item.claims['P1476']
     title_json= title_claim[0].toJSON()
     title= title_json['mainsnak']['datavalue']['value']['text']
-    if title[-1]=='.' or title[-1]=='*':
+    if title[-1]=='.' :#or title[-1]=='*':
         fixed_title=title[:-1].strip()
-        while fixed_title[-1]=='.' or fixed_title[-1]=='*':
-		fixed_title=fixed_title[:-1]
+        #while fixed_title[-1]=='.' or fixed_title[-1]=='*':
+	#	fixed_title=fixed_title[:-1].strip()
 	title_json['mainsnak']['datavalue']['value']['text']=fixed_title
         corrected_claim=pywikibot.Claim.fromJSON(site,title_json)     
         item.claims['P1476']=[corrected_claim]
-        item.editEntity(summary="Removing trailing dots or astrisks from the title")
-
+	item.editEntity(summary="Removing the trailing dot from the title")
+  except:
+    pass
 
